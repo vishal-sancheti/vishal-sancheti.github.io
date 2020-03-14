19ba617b4df5ef66a1acb81642e055321ec9b75d
Vishal Sancheti <vishky.sancheti@gmail.com>
2020-03-14 11:28:52 +0530

    new design

---
+++ README.md
@@ -0,0 +1,3 @@
+# [Personal Website](https://vishal.pro)
+
+Created using [Resume](https://startbootstrap.com/template-overviews/resume/) theme for [Bootstrap](http://getbootstrap.com/) created by [Start Bootstrap](http://startbootstrap.com/).
--- js/script.js
+++ js/script.js
@@ -1,34 +1,28 @@
-// Some code thanks to @chrisgannon
+(function($) {
+    "use strict"; // Start of use strict
 
-var select = function(s) {
-    return document.querySelector(s);
-}
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
 
-function randomBetween(min,max)
-{
-    var number = Math.floor(Math.random()*(max-min+1)+min);
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
 
-    if ( number !== 0 ){
-        return number;
-    }else {
-        return 0.5;
-    }
-}
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#sideNav'
+    });
 
-var tl = new TimelineMax();
-
-for(var i = 0; i < 20; i++){
-
-    var t = TweenMax.to(select('.bubble' + i), randomBetween(1, 1.5), {
-        x: randomBetween(12, 15) * (randomBetween(-1, 1)),
-        y: randomBetween(12, 15) * (randomBetween(-1, 1)),
-        repeat:-1,
-        repeatDelay:randomBetween(0.2, 0.5),
-        yoyo:true,
-        ease:Elastic.easeOut.config(1, 0.5)
-    })
-
-    tl.add(t, (i+1)/0.6)
-}
-
-tl.seek(50);
+})(jQuery); // End of use strict
